2899298bedc7886c4f9efaa76011eccb563e6cff
Flávio Frank <107640665+flaviofrb@users.noreply.github.com>
2024-03-02 03:01:28 -0300

    Add files via upload

---
+++ Explicação_aula.md
@@ -0,0 +1,15 @@
+Nesta aula, aprendi alguns detalhes importantes, porém muitas vezes ignorados, quando fazendo ações simples com Cypress, tais como cliques em elementos.
+
+Pré-requisitos
+É necessário ter o Node.js e o npm instalados para executar este projeto.
+
+Utilizei as versões v18.15.0 e 9.5.0 do Node.js e npm, respectivamente. Sugiro que você utilize as mesmas versões LTS ou mais recentes.
+
+Instalação
+Execute npm install (ou npm i para a versão curta) para instalar as dependências de desenvolvimento.
+
+Testes
+Execute npm test (ou npm t para a versão curta) para executar o teste em modo headless.
+
+Ou, execute npm run cy:open para abrir o Cypress no modo interativo.
+
---
+++ cypress/e2e/click.cy.js
@@ -0,0 +1,76 @@
+describe('click.cy.js', () => {
+  beforeEach(() => {
+    cy.visit('./index.html');
+  });
+
+  context('Only clicks on existing, visible and/or enabled buttons', () => {
+    it('"Click me" button', () => {
+      cy.contains('button', 'Click me')
+        .should('be.visible') // https://docs.cypress.io/guides/core-concepts/retry-ability#Why-are-some-commands-NOT-retried // on.cypress.io/interacting-with-elements
+        .click(); // show commeting the above line and forcing the click.
+
+      cy.contains('p', 'Yo!').should('be.visible');
+    })
+
+    it('"Hey, click me too!" button', () => {
+      cy.contains('button', 'click me too')
+        .should('exist') // This is an extra assertion, but the code should work without it // This could also be a .should('be.visible')
+        .click() // on.cypress.io/interacting-with-elements
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+
+    it('"Try me" button', () => {
+      cy.contains('button', 'Try me')
+        .should('be.enabled') // Esta é uma afirmação extra, mas o código deve funcionar sem ela
+        .click()
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+
+  context("Forces click before the button's visibility", () => {
+    it('"Click me" button - force: true', () => {
+      cy.contains('button', 'Click me')
+        .click({ force: true })
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+
+    it('"I\'m, hidden" button - force: true', () => {
+      cy.contains('button', "I'm hidden")
+        .click({ force: true })
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+
+  context('Clicks on the button through its icon', () => {
+    it('🏠 button', () => {
+      cy.get('#home').click() // https://docs.cypress.io/guides/core-concepts/interacting-with-elements#Covering
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+
+  context('Shows the button before clicking on it', () => {
+    it('"I\'m hidden" button', () => {
+      cy.get('#hidden-button')
+        .invoke('attr', 'style', 'visibility:visible')
+        .find('button')
+        .invoke('text', 'I was hidden')
+        .click()
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+
+    it('"Click me" button', () => {
+      cy.get('#not-visible-button')
+        .invoke('show')
+        .find('button:contains(Click me)')
+        .click()
+
+      cy.contains('p', 'Yo!').should('be.visible')
+    })
+  })
+})
